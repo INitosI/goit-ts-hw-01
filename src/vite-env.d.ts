bc9a852a5cc152f7bb7675f5396cf6d100090393
Kadurin Danylo <danil.kadurin.person@gmail.com>
2026-03-01 23:13:46 +0100

    1

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  void import('./task-1');
-  void import('./task-2');
-  void import('./task-3');
-  void import('./task-4');
-  void import('./task-5');
-  void import('./task-6');
-  void import('./task-7');
-  void import('./task-8');
-});
+/// <reference types="vite/client" />
